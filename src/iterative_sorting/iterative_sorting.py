507018f0d591819157de9cf2b4ffdb1f699d00bb
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-07 11:59:35 +0200

    Completed Bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -25,14 +25,30 @@ def selection_sort( arr ):
 
     return arr
 
-selection_sort([2, 9, 1, 2, 3, 6, 2, 12, 1])
+# selection_sort([2, 9, 1, 2, 3, 6, 2, 12, 1])
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    # Loop over every value in the array
+    for i in range(len(arr)):
+        print(f"\n i = {i}")
+        # For every value, we want to move accross the rest of the array 
+        # Until we reach the end or come accross a value that's larger than the current one
+        for j in range(0, len(arr) - i - 1):
+            print(f" j = {j}")
+            # We want to check if the value to the right is smaller than the current value
+            if arr[j] > arr[j+1]:
+                print(f"\n{arr[j]} is larger than {arr[j+1]} and should be switched") 
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+                print(arr)
 
+    # If no swap is performed, the array has been sorted
     return arr
 
+bubble_sort([3, 3, 1, 4, 2])
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
